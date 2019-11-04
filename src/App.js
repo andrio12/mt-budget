f5eb839fad1371fb2785e295cf06f907367822ca
Andri Olafsson <andriolafsson@gmail.com>
2019-11-04 16:01:48 +0000

    css and html done

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ function App(props) {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>MyTeam Budget Calculator</h1>
+        <h1>NBA 2K20 MyTeam Budget Calculator</h1>
+        <h6>A simple tool to see how much MT per game your squad would cost you</h6>
       </header>
       <Calculator name="Andri Olafsson" />
     </div>
--- src/Card.jsx
+++ src/Card.jsx
@@ -5,7 +5,8 @@ class Card extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            number: 0
+            number: 0,
+            gemLvlClass: "free"
         };
     }
 
@@ -14,6 +15,7 @@ class Card extends React.Component {
         let gemLvl = (n <= 10) ? n : 0;
         this.setState({ number: gemLvl });
         this.props.calculate(this.props.player, gemLvl);
+        this.setGemLvlClassName(gemLvl);
     }
 
     subtract() {
@@ -21,13 +23,51 @@ class Card extends React.Component {
         let gemLvl = (n >= 0) ? n : 10;
         this.setState({ number: gemLvl });
         this.props.calculate(this.props.player, gemLvl);
+        this.setGemLvlClassName(gemLvl);
+    }
+
+    setGemLvlClassName(gemLvl) {
+        switch (gemLvl) {
+            case 1:
+                this.setState({ gemLvlClass: "bronze" });
+                break;
+            case 2:
+                this.setState({ gemLvlClass: "silver" });
+                break;
+            case 3:
+                this.setState({ gemLvlClass: "gold" });
+                break;
+            case 4:
+                this.setState({ gemLvlClass: "emerald" });
+                break;
+            case 5:
+                this.setState({ gemLvlClass: "saphire" });
+                break;
+            case 6:
+                this.setState({ gemLvlClass: "ruby" });
+                break;
+            case 7:
+                this.setState({ gemLvlClass: "amethyst" });
+                break;
+            case 8:
+                this.setState({ gemLvlClass: "diamond" });
+                break;
+            case 9:
+                this.setState({ gemLvlClass: "pink-diamond" });
+                break;
+            case 10:
+                this.setState({ gemLvlClass: "galaxy-opal" });
+                break;
+            default:
+                this.setState({ gemLvlClass: "free" });
+        }    
     }
 
     render() {
         return (
-            <section className="card">
+            <section className={this.state.gemLvlClass}>
                 <button onClick={(e) => this.add(e)}>+</button>
-                <p className="mt-amount">{this.state.number}</p>
+                <p className="gem-level">{this.state.gemLvlClass}</p>
                 <button onClick={(e) => this.subtract(e)}>-</button>
             </section>
         )
